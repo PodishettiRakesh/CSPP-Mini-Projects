bbb3f649836d1a116261b709a22d3fd4b94a953a
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-05-17 10:45:27 +0530

    Main function implemented to play the memory game

--- Memory.py
+++ Memory.py
@@ -39,3 +39,44 @@ def pickedCard(dic, card1, card2):
         print(dic[card1], dic[card2])
         print("You picked unpaired cards. Please try again.")
         return False
+    
+
+def play():
+    """Main function to play the memory game."""
+    print("Welcome to the memory game")
+    board = generateBoard()
+    dic = {
+        "00": board[0], "01": board[1], "02": board[2], "03": board[3],
+        "10": board[4], "11": board[5], "12": board[6], "13": board[7],
+        "20": board[8], "21": board[9], "22": board[10], "23": board[11],
+        "30": board[12], "31": board[13], "32": board[14], "33": board[15]
+    }
+    
+    print(dic)
+    print('''The board is a 4x4 matrix, each card position is a combination of row and column.
+              For example, the first element's position is 00 which is row=0:col=0,
+              and the second element's position is 01 which is row=0:col=1.''')
+
+    revealed_pairs = 0
+    while revealed_pairs < 8:
+        print("You need to reveal all pairs to win the game.")
+        print("Enter 'exit' to end the game.")
+        display_board(board)
+        
+        choice1 = input("Enter your card1 position to open: ")
+        if choice1.lower() == "exit":
+            break
+        choice2 = input("Enter your card2 position to open: ")
+        if choice2.lower() == "exit":
+            break
+      
+        if pickedCard(dic, choice1, choice2):
+            revealed_pairs += 1
+            card = dic[choice1]
+            markNumber(board, card)
+            display_board(board)
+            print(f"Pairs revealed: {revealed_pairs}")
+            
+    print("You correctly picked all the pairs! Congratulations!")
+
+play()
