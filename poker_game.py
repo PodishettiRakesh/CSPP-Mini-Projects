25222d234b52655eff7ba240384dab865c8bf327
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-05-22 23:10:53 +0530

    initializes the cards with values

--- poker_game.py
+++ poker_game.py
@@ -19,3 +19,4 @@
 # example, both players have a pair of queens, then highest cards in each hand are compared; if the highest 
 # cards tie then the next highest cards are compared, and so on. 
 
+card_values = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, 'T': 10, 'J': 11, 'Q': 12, 'K': 13, 'A': 14}
