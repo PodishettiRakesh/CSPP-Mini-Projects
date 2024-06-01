3371d35198192987299b0d534d0423905fab7587
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-06-01 11:17:47 +0530

    initialised the cash and also number of days simulation to run

---
+++ Trading.py
@@ -0,0 +1,6 @@
+def main():
+    initial_cash = 10000
+    days_of_simulation = 5
+    stocks = [{'name': 'AAPL', 'price': 150, 'previous_price': 150},
+              {'name': 'GOOG', 'price': 2800, 'previous_price': 2800},
+              {'name': 'TSLA', 'price': 700, 'previous_price': 700}]
