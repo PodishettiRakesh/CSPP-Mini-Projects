b8be09fc5bfb915dcac9ba554db816b1e9c995f9
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-06-03 22:00:54 +0530

    fun to check the game over condition

--- 2048.py
+++ 2048.py
@@ -95,3 +95,15 @@ def check_win(grid):
             if grid[i][j] == 2048:
                 return True
     return False
+
+# Step 11: Check game over
+def check_game_over(grid):
+    for i in range(4):
+        for j in range(4):
+            if grid[i][j] == 0:
+                return False
+            if i < 3 and grid[i][j] == grid[i+1][j]:
+                return False
+            if j < 3 and grid[i][j] == grid[i][j+1]:
+                return False
+    return True
